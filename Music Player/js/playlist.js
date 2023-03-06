ec667fd4a657e9af23c793d8f5ea2c9c7bda58a5
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2023-03-06 22:33:49 +0530

    music_player

---
+++ Music Player/js/playlist.js
@@ -0,0 +1,61 @@
+let songs = [
+  {
+    name: "52 Bars",
+    artist: "Karan aujla",
+    img: "img1",
+    audio: "52 Bars"
+  },
+  {
+    name: "295(sidhu)",
+    artist: "Sidhu Moosewala",
+    img: "img2",
+    audio: "295(sidhu)"
+  },
+  {
+    name: "Everybody_Hurts",
+    artist: "Sidhu Moosewala",
+    img: "img3",
+    audio: "Everybody_Hurts"
+  },
+  {
+    name: "Levels",
+    artist: "Sidhu Moosewala",
+    img: "img4",
+    audio: "Levels"
+  },{
+    name: "On Top",
+    artist: "Karan Aujla",
+    img: "img5",
+    audio: "OnTop"
+  },
+  {
+    name: "Players",
+    artist: "Karan Aujla",
+    img: "img6",
+    audio:"Players"
+  },
+  {
+    name: "Schedule",
+    artist: "Tegi Pannu",
+    img: "img7",
+    audio:"Schedule"
+  },
+  {
+    name: "The_Last_Ride",
+    artist: "Sidhu Moosewala",
+    img: "img8",
+    audio:"The_Last_Ride"
+  },
+  {
+    name: "Untouchable",
+    artist: "Tegi Pannu",
+    img: "img9",
+    audio:"Untouchable"
+  },
+  {
+    name: "White_Brown_Black",
+    artist: "Karan Aujla",
+    img: "img10",
+    audio:"White_Brown_Black"
+  },
+]
